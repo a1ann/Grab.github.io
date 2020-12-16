eec4b77f39dd32ccac4c5b1bee7222f5a5bbf97d
a1ann <alanngomel@gmail.com>
2020-12-16 20:29:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+/*let firstLi = document.querySelector('.first-ul');
+let newLi = document.createElement('input');
+
+/*firstLi.onsubmit = function (evt) {
+  evt.preventDefault();
+  firstLi.append(newLi);
+} */
+/*firstLi.onsubmit = function (evt) {
+  evt.preventDefault();
+  let newLi = document.createElement('input');
+  commentList.append(newLi);
+};
+
+let newElement = document.createElement('input');
+firstLi.append(newElement);
+*/
+/*let InputList = document.querySelector('.input-list');
+let addInput = document.
